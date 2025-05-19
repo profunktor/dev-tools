7a2649ae2d2c1a25a9e116821a47304ac8e11715
Gabriel Volpe <volpegabriel@gmail.com>
2025-05-19 10:17:42 +0200

    make old jekyll available for sbt

--- flake.nix
+++ flake.nix
@@ -3,20 +3,25 @@
 
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-unstable";
+    nixpkgs-jekyll.url = github:NixOS/nixpkgs?rev=9a9dae8f6319600fa9aebde37f340975cab4b8c0;
     flake-utils.url = github:numtide/flake-utils;
   };
 
-  outputs = { nixpkgs, flake-utils, ... }:
+  outputs = { nixpkgs, nixpkgs-jekyll, flake-utils, ... }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         scalaOverlay = self: super: {
+          # jekyll v4.3.1 is needed for sbt-microsite
+          inherit (nixpkgs-jekyll.legacyPackages.${system}) jekyll;
           jre = super.jdk21_headless;
           sbt = super.sbt.overrideAttrs (
             old: {
               nativeBuildInputs = old.nativeBuildInputs or [ ] ++ [ super.makeWrapper ];
               # Setting SBT_OPTS because of this bug: https://github.com/sbt/sbt-site/issues/169
               postInstall = ''
-                wrapProgram $out/bin/sbt --suffix SBT_OPTS : '--add-opens java.base/java.lang=ALL-UNNAMED'
+                wrapProgram $out/bin/sbt \
+                  --suffix SBT_OPTS : '--add-opens java.base/java.lang=ALL-UNNAMED' \
+                  --prefix PATH : ${super.lib.makeBinPath [ self.jekyll ]}
               '';
             }
           );
